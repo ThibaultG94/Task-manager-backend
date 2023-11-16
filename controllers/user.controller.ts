dfeab622dcb898b498c4a6f9039cf95623dd51a7
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-11-16 19:30:06 +0100

    Update logoutUser function in user.controller.ts
    - Add a check to handle the case when no token or refresh token is available before deleting
    - Return an error response with status 400 and a message indicating that no token is available
    - Refactor the code to improve readbility and error handling
    - Update the success response message when a user is logged out successfully

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -470,6 +470,10 @@ export const logoutUser = async (
 			res.clearCookie('token');
 		}
 
+		if (!refreshToken && !token) {
+			return res.status(400).json({ message: 'No token to delete' });
+		}
+
 		res.status(200).json({ message: 'User logged out successfully' });
 	} catch (err) {
 		res.status(500).json({ message: 'Internal server error' });
