bda1d09ee4ea5eedda2c8197b5f956da4bae57e6
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-02-12 17:29:21 +0100

    Add endpoints to retrieve sent and received workspace invitations

--- controllers/workspaceInvitation.controller.ts
+++ controllers/workspaceInvitation.controller.ts
@@ -50,3 +50,121 @@ export const sendInvitationWorkspace = async (
 		res.status(500).json({ message: 'Internal server error' });
 	}
 };
+
+// Endpoint to retrieve sent invitations
+export const getSentOutWorkspaceInvitations = async (
+	req: express.Request,
+	res: express.Response
+) => {
+	try {
+		const userId = req.params.id;
+		const user = await userModel.findById(userId);
+
+		if (!user) {
+			return res.status(400).json({ message: 'User does not exist' });
+		}
+
+		const invitationsSentOut = await workspaceInvitationModel.find({
+			senderId: userId,
+		});
+
+		const invitationsInformations = await Promise.all(
+			invitationsSentOut.map(async (invitation) => {
+				const guest = await userModel.findById(invitation.guestId);
+				const workspace = await workspaceModel.findById(
+					invitation.workspaceId
+				);
+				if (!guest || !workspace) {
+					return res.status(400).json({
+						message: 'Guest or workspace does not exist',
+					});
+				}
+				return {
+					invitationId: invitation._id,
+					guestEmail: guest?.email,
+					guestUsername: guest?.username,
+					role: invitation.role,
+					status: invitation.status,
+					workspace,
+				};
+			})
+		);
+
+		const invitationsPending = invitationsInformations.filter(
+			(invitation) =>
+				invitation.status === 'PENDING' ||
+				invitation.status === 'REJECTED'
+		);
+		const invitationsAccepted = invitationsInformations.filter(
+			(invitation) => invitation.status === 'ACCEPTED'
+		);
+
+		const invitations = {
+			pending: invitationsPending,
+			accepted: invitationsAccepted,
+		};
+
+		return res.status(200).json({ workspaceInvitations: invitations });
+	} catch (error) {
+		res.status(500).json({ message: 'Internal server error' });
+	}
+};
+
+// Endpoint to retrieve received invitations
+export const getReceivedWorkspaceInvitations = async (
+	req: express.Request,
+	res: express.Response
+) => {
+	try {
+		const userId = req.params.id;
+		const user = await userModel.findById(userId);
+
+		if (!user) {
+			return res.status(400).json({ message: 'User does not exist' });
+		}
+
+		const invitationsReceived = await workspaceInvitationModel.find({
+			guestId: userId,
+		});
+
+		// Transformer les invitations en utilisant Promise.all
+		const invitationsInformations = await Promise.all(
+			invitationsReceived.map(async (invitation) => {
+				const sender = await userModel.findById(invitation.senderId);
+				const workspace = await workspaceModel.findById(
+					invitation.workspaceId
+				);
+				if (!sender || !workspace) {
+					return res.status(400).json({
+						message: 'Guest or workspace does not exist',
+					});
+				}
+				return {
+					invitationId: invitation._id,
+					senderEmail: sender?.email,
+					senderUsername: sender?.username,
+					role: invitation.role,
+					status: invitation.status,
+					workspace,
+				};
+			})
+		);
+
+		// Filtrer les invitations
+		const invitationsPending = invitationsInformations.filter(
+			(invitation) => invitation.status === 'PENDING'
+		);
+		const invitationsAccepted = invitationsInformations.filter(
+			(invitation) => invitation.status === 'ACCEPTED'
+		);
+
+		const invitations = {
+			pending: invitationsPending,
+			accepted: invitationsAccepted,
+		};
+
+		return res.status(200).json({ invitations });
+	} catch (error) {
+		res.status(500).json({ message: 'Internal server error' });
+	}
+};
--- routes/workspaceInvitation.routes.ts
+++ routes/workspaceInvitation.routes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import { auth } from '../middlewares/auth.middlewares';
-import { sendInvitationWorkspace } from '../controllers/workspaceInvitation.controller';
+import {
+	getReceivedWorkspaceInvitations,
+	getSentOutWorkspaceInvitations,
+	sendInvitationWorkspace,
+} from '../controllers/workspaceInvitation.controller';
 
 const router = express.Router();
 
 // Route to send a workspace invitation
 router.post('/send-invitation', auth, sendInvitationWorkspace);
+
+// Route to retrieve sent workspaces invitations
+router.get('/sentout-invitations/:id', auth, getSentOutWorkspaceInvitations);
+
+// Route to retrieve received invitations
+router.get('/received-invitations/:id', auth, getReceivedWorkspaceInvitations);
 
 export default router;
