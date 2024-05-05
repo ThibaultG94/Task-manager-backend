0652b0229e623237db76118d5f7275687954d9c9
Thibault <thibault.guilhem@gmail.com>
2024-05-05 11:28:46 +0200

    Enrich workspace create response with  information and workspace details

--- controllers/workspace.controller.ts
+++ controllers/workspace.controller.ts
@@ -158,7 +158,53 @@ export const createWorkspace = async (
 			members: req.body.members,
 		});
 
-		res.status(200).json({ workspace: workspace });
+		let workspaces = await workspaceModel
+			.find({
+				$or: [{ userId }, { 'members.userId': userId }],
+			})
+			.sort({ lastUpdateDate: -1 })
+			.lean();
+
+		const memberIds = [
+			...new Set(
+				workspaces.flatMap((workspace) =>
+					workspace.members.map((member) => member.userId.toString())
+				)
+			),
+		];
+		const users = await userModel
+			.find({
+				_id: {
+					$in: memberIds.map((id) => new mongoose.Types.ObjectId(id)),
+				},
+			})
+			.lean();
+
+		const usersMap = users.reduce<{ [key: string]: UserInfo }>(
+			(acc, user) => {
+				acc[user._id.toString()] = {
+					username: user.username,
+					email: user.email,
+				};
+				return acc;
+			},
+			{}
+		);
+
+		workspaces = workspaces.map((workspace) => {
+			const enrichedMembers = workspace.members.map((member) => {
+				const userInfo = usersMap[member.userId.toString()];
+				return {
+					userId: member.userId,
+					role: member.role,
+					username: userInfo?.username,
+					email: userInfo?.email,
+				};
+			});
+			return { ...workspace, members: enrichedMembers };
+		});
+
+		return res.status(200).json({ workspaces: workspaces });
 	} catch (error) {
 		res.status(500).json({ message: 'Internal server error' });
 	}
